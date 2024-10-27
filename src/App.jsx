0ef543d85ec9a8ec8cc26524df878c17b9e50d9b
Miha0809 <miha080104@gmail.com>
2024-10-27 20:33:17 +0100

    Lesson 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
-import Card from "./components/Card";
+import Card from "./components/Card/Card";
 import Drawer from "./components/Drawer";
 import Header from "./components/Header";
+
+const array = [
+  {
+    title:"Чоловічі кросівки Nike Blazer Mid Suede",
+    price: 12999,
+    imageUrl: "/img/sneakers/1.jpg"
+  },
+  {
+    title: "Чоловічі кросівки Nike Air Max 270",
+    price: 12345,
+    imageUrl: "/img/sneakers/2.jpg"
+  }
+]
 
 function App() {
   return (
@@ -18,7 +31,16 @@ function App() {
         </div>
 
         <div className="d-flex">
-          <Card />
+          {
+            array.map(obj => (
+              <Card
+                title={obj.title}
+                price={obj.price}
+                imageUrl={obj.imageUrl}
+                onClick={console.log(obj)}
+              />
+            ))
+          }
 
           {/*  */}
         </div>
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,19 @@
-function Card() {
+import styles from "./Card.module.scss";
+
+function Card(props) {
   return (
-    <div className="card">
-      <div className="favorite">
+    <div className={styles.card}>
+      <div className={styles.favorite}>
         <img src="/img/heart-unlike.svg" alt="unliked" />
       </div>
-      <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers" />
-      <h5>Чоловічі кросівки Nike Blazer Mid Suede</h5>
+      <img width={133} height={112} src={props.imageUrl} alt="Sneakers" />
+      <h5>{props.title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column">
           <span>Ціна:</span>
-          <b>12 999грн.</b>
+          <b>{props.price} грн.</b>
         </div>
-        <button className="button">
+        <button className="button" onClick={() => props.onClick}>
           <img width={11} height={11} src="/img/button.svg" alt="add to cart" />
         </button>
       </div>
